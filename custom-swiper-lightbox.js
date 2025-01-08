c328a3f8e3b027e8a36bea2153866f124188a52f
ArmiinA <122403724+ArmiinA@users.noreply.github.com>
2025-01-08 05:20:05 +0330

    Update custom-swiper-lightbox.js

--- custom-swiper-lightbox.js
+++ custom-swiper-lightbox.js
@@ -1,1 +1,23 @@
 
+jQuery(document).ready(function($) {
+    // Initialize Swiper
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // Initialize Lightbox
+    $('.lightbox').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true,
+        },
+    });
+});
